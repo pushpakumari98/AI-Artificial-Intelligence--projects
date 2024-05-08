3bcb18468496d44126ace8d57b9db6e36f871d15
Pushpa Kumari <153999245+pushpakumari98@users.noreply.github.com>
2024-05-08 21:09:56 +0530

    Update 2_FaceDetection.py

--- 2_FaceDetection.py
+++ 2_FaceDetection.py
@@ -1,34 +1,23 @@
 import cv2
-import os
-dataset="Python"         #---->folder name
-name="FaceDetection"     #---->file name 
-
-path=os.path.join(dataset,name)
-if not os.path.isdir(path):
-       os.makedirs(path)
        
-(width,height)=(130,100)   
-alg="C:/Users/panka/Desktop/Pushpa program/Python/haarcascade_frontalface_alt.xml"
-haar_cascade=cv2.CascadeClassifier(alg)
-cam=cv2.VideoCapture(2)
+   
+alg="haarcascade_frontalface_alt.xml" #Loading harcascade face Algorithm
+haar_cascade=cv2.CascadeClassifier(alg) #load the algorithm inside computer vision in the variable
+cam=cv2.VideoCapture(2) #initialising camera
 
-count=1
-while count<31:
-    print(count)
-    _,img=cam.read()
-    grayImg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    face=haar_cascade.detectMultiScale(grayImg,1.3,4)
+
+
+while True:
+    _,img=cam.read() #reading frame from camera
+    grayImg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #convert color image into grayscale
+    face=haar_cascade.detectMultiScale(grayImg,1.3,4) #to get coordinate of face
     for (x,y,w,h) in face:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        faceOnly=grayImg[y:y+h,x:x+w]
-        resizeImg=cv2.resize(faceOnly,(width,height))
-        cv2.imwrite("%s/%s.jpg"%(path,count),resizeImg)
-        count+=1
+          cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)#drawing rectangle on the face algorithm
         
-    cv2.imshow("FaceDetection",img)
+    cv2.imshow("FaceDetection",img) #display the output frame
     key=cv2.waitKey(10)
-    if key == 27:
+    if key == 27: #27 is for pressing an escape(Esc) button
         break
-print("Image captured successfully")
 cam.release()
 cv2.destroyAllWindows()
+
